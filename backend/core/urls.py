823a3202450f9c43c7cfe3758f73adc3da35073a
shayanpzp <shayanpzp1@gmail.com>
2024-01-21 18:48:49 -0800

    chore : create and published the product list page(html file)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from core.views import index
+from core.views import index, category_list_view, product_list_view
 
 app_name = "core"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("category/", category_list_view, name="category_list"),
+    path("products/", product_list_view, name="product_list")
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -15,4 +15,26 @@ def index(request):
     return render(request, "core/index.html",context)
 
 
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published")
+    
+    context = {
+        "products" : products
+    }
+    return render(request, "core/product_list.html",context)
+
+
+
+
+def category_list_view(request): 
+    # products = Product.objects.all().order_by("-id")
+    categories = Category.objects.all
+    
+    context = {
+        "categories" : categories
+    }
+    return render(request, "core/category_list.html",context)
+
+
     
