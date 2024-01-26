f937ce0b4ce2f05685d49fad4f8ce1b8002709a3
shayanpzp <shayanpzp1@gmail.com>
2024-01-25 22:56:46 -0800

    fix : fixing simpleLazyObject problem.

--- backend/core/context_processor.py
+++ backend/core/context_processor.py
@@ -1,11 +1,16 @@
 from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImage, ProductReview, Wishlist, Address    
 
-
 def default(request):
     categories = Category.objects.all()
-    address = Address.objects.get(user=request.user)
+    address = None 
+
+    if request.user.is_authenticated:
+        try:
+            address = Address.objects.get(user=request.user)
+        except Address.DoesNotExist:
+            pass
     
     return {
-        "categories" : categories,  
-        "address" : address,  
+        "categories" : categories,
+        "address" : address,
     }
