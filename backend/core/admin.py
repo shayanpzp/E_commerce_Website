73c27a56ee471fd6f7eb744f73ae53e2162a0e72
shayanpzp <shayanpzp1@gmail.com>
2024-02-01 11:45:35 -0800

    chore : add filter section & search in admin panel.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -9,22 +9,24 @@ class ProductImageAdmin(admin.TabularInline):
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageAdmin]
     list_display = ['user', 'title', 'product_image', 'price', 'vendor', 'category', 'featured', 'product_status', 'pid']
-    
+    list_filter =  ['user' , 'title']
+    sortable_by =  ['price']
     
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'category_image']
     
+    
 class VendorAdmin(admin.ModelAdmin):
     list_display = ['title', 'vendor_image']
-    
+    list_filter =  ['title' ]
     
 class CartOrderAdmin(admin.ModelAdmin):
     list_display = ['user', 'price', 'paid_status', 'order_date', 'product_status']
-    
+    list_filter =  ['price' , 'paid_status' , 'product_status']
     
 class CartOrderItemsAdmin(admin.ModelAdmin):
     list_display = ['order', 'invoice_no', 'item', 'image', 'quantity', 'price', 'total']
-    
+    list_filter =  ['item' , 'quantity' , 'price']
 
 class ProductReviewAdmin(admin.ModelAdmin):
     list_display = ['user', 'product', 'review', 'rating']
