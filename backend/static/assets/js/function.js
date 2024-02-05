572f59e574a1cd777fcd4247ccdc0a076e16419c
shayanpzp <shayanpzp1@gmail.com>
2024-02-04 17:34:30 -0800

    chore : Filter Product By Price

--- backend/static/assets/js/function.js
+++ backend/static/assets/js/function.js
@@ -66,10 +66,16 @@ $("#commentForm").submit(function(e){
 
 
 $(document).ready(function (){
-    $(".filter-checkbox").on("click" , function(){
+    $(".filter-checkbox, #price-filter-btn").on("click" , function(){
         console.log("A check box has clicked")
 
         let filter_object = {}
+
+        let min_price = $("#max_price").attr("min")
+        let max_price = $("#max_price").val()
+
+        filter_object.min_price = min_price;
+        filter_object.max_price = max_price;
 
         $(".filter-checkbox").each(function(){
             let filter_value = $(this).val()
