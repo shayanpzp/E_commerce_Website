66c58722813aca2f659213595faaf8ec788c6d1b
shayanpzp <shayanpzp1@gmail.com>
2024-02-05 22:31:54 -0800

    chore : set Update Product Quantity from Cart Page section.

--- backend/static/assets/js/function.js
+++ backend/static/assets/js/function.js
@@ -192,6 +192,34 @@ $(document).ready(function (){
             }
         })
     })
+
+    $(".update-product").on("click",function(){
+
+        let product_id = $(this).attr("data-product")
+        let this_val = $("this")
+        let product_quantity = $(".product-qty-"+product_id).val()
+
+        console.log("pid: ", product_id)
+
+
+
+        $.ajax({
+            url:'/update-cart',
+            data: {
+                'id' : product_id,
+                'qty' : product_quantity,
+            },
+            dataType:'json',
+            beforeSend:function(){
+                this_val.hide()
+            },
+            success:function(response){
+                this_val.show()
+                $(".cart-items-count").text(response.totalcartitems)
+                $("#cart-list").html(response.data)
+            }
+        })
+    })
 })
 
 
